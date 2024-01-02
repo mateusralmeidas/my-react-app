0658773cab67e39398d5ad3e3717ed1059f29490
Mateus Solar <mateusr.almeidas@gmail.com>
2024-01-02 17:45:34 -0300

    Refreshing data on real time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import {useEffect,useState} from 'react'
 import {db} from './firebaseConnection'
-import {doc, setDoc, collection, addDoc, getDoc, getDocs} from 'firebase/firestore'
+import {doc,
+   setDoc, 
+   collection,
+    addDoc,
+      getDoc, 
+      getDocs, 
+      updateDoc, 
+      deleteDoc,
+    onSnapshot} from 'firebase/firestore'
 import './app.css'
 
 function App() {
@@ -8,6 +16,28 @@ function App() {
   const [title,setTitle] = useState('');
   const [author,setAuthor] = useState('');
   const [posts,setPosts] = useState([]);
+  const [idPost,setIdPost] = useState('');
+
+  useEffect(()=>{
+    async function loadPosts(){
+      const unsub = onSnapshot(collection(db,"posts"),(snapshot)=>{
+        let listPosts = [];
+
+        snapshot.forEach((doc)=>{
+          listPosts.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor
+          })
+        })
+
+        setPosts(listPosts);
+      })
+    }
+
+    loadPosts();
+  },[])
+
 
   async function handleAdd(){
     // await setDoc(doc(db, "posts","12345"),{
@@ -69,11 +99,44 @@ function App() {
     })
   }
 
+  async function editarPost(){
+
+    const docRef = doc(db, "posts", idPost)
+    await updateDoc(docRef,{
+      titulo: title,
+      autor: author
+    })
+    .then(()=>{
+      setIdPost('');
+      setTitle('');
+      setAuthor('');
+    })
+    .catch((error)=>{
+      console.log("Gerou erro: " + error)
+    })
+  }
+
+  async function excluirPost(id){
+    const docRef = doc(db, "posts", id)
+    await deleteDoc(docRef)
+    .then(()=>{
+      alert("Excluido com sucesso!")
+    })
+    .catch((error)=>{
+      console.log("Gerou erro: " + error)
+    })
+  }
+
   return (
     <div className="App">
       <h1>React + Firebase</h1>
 
       <div className="container">
+
+
+        <label>Id do Post:</label>
+        <input type="text" placeholder='Digite o Id do post' value={idPost} onChange={(e) => setIdPost(e.target.value)}/>
+        <br/>
         <label>Titulo:</label>
         <textarea type="text" placeholder='Digite o título' value={title} onChange={(e) => setTitle(e.target.value)}/>
 
@@ -84,13 +147,16 @@ function App() {
 
         <button onClick={buscarPost}>Buscar post</button>
 
+        <button onClick={editarPost}>Atualizar post</button>
 
         <ul>
           {posts.map((post)=>{
             return(
               <li key={post.id}>
+                <strong>ID: {post.id}</strong><br/>
                 <span>Titulo: {post.titulo}</span> <br/>
-                <span>Autor: {post.autor}</span> <br/><br/>
+                <span>Autor: {post.autor}</span> <br/>
+                <button onClick={()=> excluirPost(post.id)}>Excluir</button> <br/><br/>
               </li>
             );
           })}
